79cb2b1aac60835b7c6630fd121567c139c4c8e6
Dillon Kearns <dillon@dillonkearns.com>
2019-03-29 12:39:51 -0700

    Show product image previews

--- src/Product.elm
+++ src/Product.elm
@@ -11,16 +11,20 @@ import RemoteData exposing (RemoteData)
 
 
 type Product
-    = Product Api.Scalar.ProductCode String
+    = Product Api.Scalar.ProductCode String String
 
 
 selection : SelectionSet.SelectionSet Product Api.Object.Product
 selection =
-    SelectionSet.map2 Product
+    SelectionSet.map3 Product
         Api.Object.Product.code
         Api.Object.Product.name
+        Api.Object.Product.imageUrl
 
 
 view : Product -> Element msg
-view (Product (Api.Scalar.ProductCode rawCode) productName) =
-    Element.text productName
+view (Product (Api.Scalar.ProductCode rawCode) productName imageUrl) =
+    Element.row [ Element.spacing 30 ]
+        [ Element.image [ Element.width (Element.px 200) ] { src = imageUrl, description = productName }
+        , Element.text productName
+        ]
