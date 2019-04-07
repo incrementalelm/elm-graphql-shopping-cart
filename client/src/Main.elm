30221357c140a0adeb6bc99e5553760911751843
Dillon Kearns <dillon@dillonkearns.com>
2019-04-06 19:57:50 -0700

    Initial wiring for SPA.

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -8,6 +8,7 @@ import Element exposing (Element)
 import Element.Border
 import Element.Events
 import Element.Input
+import Page.Home
 import Product exposing (Product)
 import RemoteData exposing (RemoteData)
 import Request exposing (Response)
@@ -15,35 +16,18 @@ import Route exposing (Route)
 import Url exposing (Url)
 
 
-productsRequest : Cmd Msg
-productsRequest =
-    Query.products Product.selection
-        |> Request.query GotProducts
-
-
-discountRequest : String -> Cmd Msg
-discountRequest discountCode =
-    Query.discountOrError { discountCode = discountCode } Discount.selection
-        |> Request.query GotResponse
-
-
 
 -- Elm Architecture Setup
 
 
 type Msg
-    = GotResponse (Response Discount)
-    | GotProducts (Response (List Product.Product))
-    | ChangedDiscountCode String
-    | ApplyDiscountCode
-    | UrlRequested Browser.UrlRequest
+    = UrlRequested Browser.UrlRequest
     | UrlChanged Url
+    | HomeMsg Page.Home.Msg
 
 
 type alias Model =
-    { discountCode : String
-    , discountInfo : Response Discount
-    , products : Response (List Product.Product)
+    { subModel : Page.Home.Model
     , navKey : Browser.Navigation.Key
     , route : Route
     }
@@ -55,31 +39,17 @@ type alias Flags =
 
 init : Flags -> Url -> Browser.Navigation.Key -> ( Model, Cmd Msg )
 init _ url navKey =
-    ( { discountCode = ""
-      , discountInfo = RemoteData.Success Discount.none
-      , products = RemoteData.Loading
+    ( { subModel = Tuple.first Page.Home.init
       , navKey = navKey
       , route = Route.Home
       }
-    , productsRequest
+    , Tuple.second Page.Home.init |> Cmd.map HomeMsg
     )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        GotResponse response ->
-            ( { model | discountInfo = response }, Cmd.none )
-
-        ChangedDiscountCode newDiscountCode ->
-            ( { model | discountCode = newDiscountCode }, Cmd.none )
-
-        GotProducts productsResponse ->
-            ( { model | products = productsResponse }, Cmd.none )
-
-        ApplyDiscountCode ->
-            ( model, discountRequest model.discountCode )
-
         UrlRequested urlRequest ->
             case urlRequest of
                 Browser.Internal url ->
@@ -93,6 +63,15 @@ update msg model =
         UrlChanged url ->
             ( { model | route = Route.Product }
             , Cmd.none
+            )
+
+        HomeMsg homeMsg ->
+            let
+                ( updatedSubModel, subMsg ) =
+                    Page.Home.update homeMsg model.subModel
+            in
+            ( { model | subModel = updatedSubModel }
+            , subMsg |> Cmd.map HomeMsg
             )
 
 
@@ -112,74 +91,8 @@ view : Model -> Browser.Document Msg
 view model =
     { title = "The Elm Shoppe"
     , body =
-        Element.column [ Element.spacing 15 ]
-            [ Element.text "Products"
-            , productsView model
-            , discountInputView model
-            , model.route |> Debug.toString |> Element.text
-            ]
+        Page.Home.view model.subModel
+            |> Element.map HomeMsg
             |> Element.layout []
             |> List.singleton
     }
-
-
-productsView : Model -> Element Msg
-productsView model =
-    case RemoteData.map2 Tuple.pair model.discountInfo model.products of
-        RemoteData.Success ( discount, products ) ->
-            List.map (Product.view discount) products
-                |> Element.column []
-
-        RemoteData.Failure error ->
-            Element.text <| Debug.toString error
-
-        RemoteData.Loading ->
-            Element.text "Loading..."
-
-        RemoteData.NotAsked ->
-            Element.text "Not asked..."
-
-
-discountInputView : Model -> Element Msg
-discountInputView model =
-    Element.row [ Element.width Element.fill, Element.spacing 20 ]
-        [ inputView model
-        , applyButton
-        , discountView model
-        ]
-
-
-applyButton : Element Msg
-applyButton =
-    Element.el
-        [ Element.Border.width 2
-        , Element.padding 10
-        , Element.Events.onClick ApplyDiscountCode
-        ]
-        (Element.text "Apply")
-
-
-discountView : { model | discountCode : String, discountInfo : RemoteData e Discount.Discount } -> Element msg
-discountView { discountCode, discountInfo } =
-    case discountInfo of
-        RemoteData.NotAsked ->
-            Element.text ""
-
-        RemoteData.Loading ->
-            Element.text "..."
-
-        RemoteData.Failure e ->
-            Element.text "Failed to load"
-
-        RemoteData.Success discount ->
-            Discount.view discount
-
-
-inputView : { a | discountCode : String } -> Element Msg
-inputView model =
-    Element.Input.text [ Element.width Element.fill ]
-        { onChange = ChangedDiscountCode
-        , text = model.discountCode
-        , placeholder = Nothing
-        , label = Element.Input.labelLeft [ Element.centerY ] (Element.text "Discount Code")
-        }
---
+++ client/src/Page/Home.elm
@@ -0,0 +1,132 @@
+module Page.Home exposing (Model, Msg, init, update, view)
+
+import Api.Query as Query
+import Discount exposing (Discount)
+import Element exposing (Element)
+import Element.Border
+import Element.Events
+import Element.Input
+import Product exposing (Product)
+import RemoteData exposing (RemoteData)
+import Request exposing (Response)
+
+
+type Msg
+    = GotResponse (Response Discount)
+    | GotProducts (Response (List Product.Product))
+    | ChangedDiscountCode String
+    | ApplyDiscountCode
+
+
+type alias Model =
+    { discountCode : String
+    , discountInfo : Response Discount
+    , products : Response (List Product.Product)
+    }
+
+
+update msg model =
+    case msg of
+        GotResponse response ->
+            ( { model | discountInfo = response }, Cmd.none )
+
+        ChangedDiscountCode newDiscountCode ->
+            ( { model | discountCode = newDiscountCode }, Cmd.none )
+
+        GotProducts productsResponse ->
+            ( { model | products = productsResponse }, Cmd.none )
+
+        ApplyDiscountCode ->
+            ( model, discountRequest model.discountCode )
+
+
+view model =
+    Element.column [ Element.spacing 15 ]
+        [ Element.text "Products"
+        , productsView model
+        , discountInputView model
+        ]
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { discountCode = ""
+      , discountInfo = RemoteData.Success Discount.none
+      , products = RemoteData.Loading
+      }
+    , productsRequest
+    )
+
+
+productsView : Model -> Element Msg
+productsView model =
+    case RemoteData.map2 Tuple.pair model.discountInfo model.products of
+        RemoteData.Success ( discount, products ) ->
+            List.map (Product.view discount) products
+                |> Element.column []
+
+        RemoteData.Failure error ->
+            Element.text <| Debug.toString error
+
+        RemoteData.Loading ->
+            Element.text "Loading..."
+
+        RemoteData.NotAsked ->
+            Element.text "Not asked..."
+
+
+discountInputView : Model -> Element Msg
+discountInputView model =
+    Element.row [ Element.width Element.fill, Element.spacing 20 ]
+        [ inputView model
+        , applyButton
+        , discountView model
+        ]
+
+
+applyButton : Element Msg
+applyButton =
+    Element.el
+        [ Element.Border.width 2
+        , Element.padding 10
+        , Element.Events.onClick ApplyDiscountCode
+        ]
+        (Element.text "Apply")
+
+
+discountView : { model | discountCode : String, discountInfo : RemoteData e Discount.Discount } -> Element msg
+discountView { discountCode, discountInfo } =
+    case discountInfo of
+        RemoteData.NotAsked ->
+            Element.text ""
+
+        RemoteData.Loading ->
+            Element.text "..."
+
+        RemoteData.Failure e ->
+            Element.text "Failed to load"
+
+        RemoteData.Success discount ->
+            Discount.view discount
+
+
+productsRequest : Cmd Msg
+productsRequest =
+    Query.products Product.selection
+        |> Request.query GotProducts
+
+
+discountRequest : String -> Cmd Msg
+discountRequest discountCode =
+    Query.discountOrError { discountCode = discountCode } Discount.selection
+        |> Request.query GotResponse
+
+
+inputView : { a | discountCode : String } -> Element Msg
+inputView model =
+    Element.Input.text [ Element.width Element.fill ]
+        { onChange = ChangedDiscountCode
+        , text = model.discountCode
+        , placeholder = Nothing
+        , label = Element.Input.labelLeft [ Element.centerY ] (Element.text "Discount Code")
+        }
